3e3173202825438857d94c278f94496863df131a
cellobeetle <emmaballif@icloud.com>
2025-01-29 18:16:02 -0700

    Update game.py
    Started and updated game

--- game.py
+++ game.py
@@ -1,1 +1,28 @@
-print("hello world")
+print("\nhello world")
+escape = ""
+
+
+
+# this is the main game loop
+print("This is the location you are in")
+while(True):
+    choice = input("\ndo you want to sit?")
+    if(choice == "yes"):
+        print("You are sitting. How pleasant it must be to sit")
+        see = input("You look out the window. It is raining. Are you cold?")
+    if(choice == "no"):
+        print("You remain standing. Your legs must be tired")
+    if(see == "yes"):
+        print("That's too bad, we only have ice cream here.")
+    elif(see == "no"):
+        print("Oh good, maybe you should eat some ice cream:)")
+    escape = input("do you want to leave?")
+    if(escape == "yes"):
+        break
+    elif(escape == "no"):
+        print("Oh good, I like having you here")
+
+
+
+    
+
